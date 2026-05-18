139d1731cda2733868aebd97f34471051b72dbfc
gygygfg <1207584579@qq.com>
2026-05-18 17:56:30 +0800

    [NeoAI] 编辑 test_init_modules.lua

--- lua/NeoAI/tests/test_init_modules.lua
+++ lua/NeoAI/tests/test_init_modules.lua
@@ -348,15 +348,21 @@ function M.run(test_module)
       assert.is_true(type(msgs) == "table", "session_to_messages 应返回表")
       assert.is_true(#msgs > 0, "应有消息")
     end,
-
     test_approval_handler_basic = function()
       local ah = require("NeoAI.tools.approval_handler")
       -- 测试基本功能：入队和清空
       ah.clear_queue()
       assert.equal(0, ah.queue_length(), "清空后队列长度应为0")
-      ah.enqueue({ name = "test_tool", arguments = {}, on_result = function() end })
+      ah.enqueue({
+        tool_name = "test_tool",
+        resolved_args = {},
+        raw_args = {},
+        on_success = function() end,
+        on_error = function() end,
+      })
       assert.is_true(ah.queue_length() > 0, "入队后队列长度应大于0")
       ah.clear_queue()
+    end,
     end,
 
     test_approval_state_basic = function()
